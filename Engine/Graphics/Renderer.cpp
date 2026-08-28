463c7be902191dc1ff1d39a8080e01ee391ab09f
HerculesGameEngine <herculesgameengine@gmail.com>
2026-08-27 21:37:56 -0300

    Create Renderer.cpp with platform-specific comments
    Added Renderer.cpp with interface documentation for various platforms.

---
+++ Engine/Graphics/Renderer.cpp
@@ -0,0 +1,16 @@
+#include "Renderer.hpp"
+
+namespace Hercules {
+
+// Renderer é uma interface.
+// As implementações específicas ficam em:
+//
+// Platforms/Windows/
+// Platforms/macOS/
+// Platforms/Nintendo3DS/
+// Platforms/Wii/
+// Platforms/GameCube/
+// Platforms/PSP/
+// Platforms/Vita/
+
+}
